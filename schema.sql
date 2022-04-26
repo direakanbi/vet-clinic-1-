82e417f50d01a5cdeaeefaed68d52c4b81b2f75f
Akanbi Dire <direakanbi@icloud.com>
2022-04-26 09:51:19 +0100

    Added schema for querying and updating data to the table

--- schema.sql
+++ schema.sql
@@ -1,9 +1,13 @@
 CREATE TABLE dbo.animal
 (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY, -- primary key column
     name string NOT NULL,
     date_of_birth DATE,
     escape_attempts INT,
     neutered boolean,
     weight_kg DECIMAL
+    -- specify more columns here
 );
+
+ALTER TABLE dbo.animal
+    ADD species VARCHAR(60);
