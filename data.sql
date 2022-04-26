711fea0bea210628a47b01216d659ffd60b32434
Akanbi Dire <direakanbi@icloud.com>
2022-04-26 08:42:43 +0100

    Inserted data in the animal table

--- data.sql
+++ data.sql
@@ -1,4 +1,8 @@
-INSERT INTO animals VALUES(1, 'Agumon', 'Feb 3, 2020',0, true, 10.23);
-INSERT INTO animals VALUES(2, 'Gabumon', 'Nov 15, 2018',2, true, 8.00);
-INSERT INTO animals VALUES(3, 'Pikachu', 'Jan 7, 2021',1, false, 15.04);
-INSERT INTO animals VALUES(4, 'Devimon', 'May 12, 2017',5, true, 11.00);
+INSERT INTO animal
+   ([id],[name],[date_of_birth],[escape_attempts],[neutered],[weight_kg])
+VALUES
+   ( 1, N'Agumon', '3-2-2020', 0, 'true', N'10.23kg'),
+   ( 2, N'Gabumon', '15-11-2018', 2, 'true', N'8kg'),
+   ( 3, N'Pikachu', '7-1-2021', 1, 'false', N'15.04kg'),
+   ( 4, N'Devimon', '12-04-2017', 5, 'true', N'11kg')
+GO
